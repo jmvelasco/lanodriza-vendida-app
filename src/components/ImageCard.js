398efd04666840cbf372c6cc33e69f3db80139ac
JManuel Velasco <jmanuel.velasco@olympicchannel.com>
2022-04-08 16:50:23 +0200

    Remove extra useEffect hook

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,19 +1,11 @@
-import { useEffect, useState } from "react"
-
 export const ImageCard = (props) => {
-  const [imageCardProps, receiveNewProps] = useState(props);
-
-  useEffect(() => {
-    receiveNewProps(props);
-  }, [receiveNewProps, props]);
-
   return (
     <div className="card-wrapper">
       <div className="info">
-        <div className="name">{imageCardProps.name}</div>
-        <div className="description">{imageCardProps.description}</div>
+        <div className="name">{props.name}</div>
+        <div className="description">{props.description}</div>
       </div>
-      <img src={imageCardProps.resourcePath} alt={imageCardProps.name} />
+      <img src={props.resourcePath} alt={props.name} />
     </div>
   )
 }
--- src/components/VideoCard.js
+++ src/components/VideoCard.js
@@ -1,20 +1,12 @@
-import React, { useEffect, useState } from 'react'
-
 export const VideoCard = (props) => {
-  const [videoCardProps, receiveNewProps] = useState(props);
-
-  useEffect(() => {
-    receiveNewProps(props);
-  }, [receiveNewProps, props]);
-
   return (
     <div className="card-wrapper">
       <div className="info">
-        <div className="name">{videoCardProps.name}</div>
-        <div className="description">{videoCardProps.description}</div>
+        <div className="name">{props.name}</div>
+        <div className="description">{props.description}</div>
       </div>
       <video width="320px" controls>
-        <source src={videoCardProps.resourcePath} type="video/mp4" />
+        <source src={props.resourcePath} type="video/mp4" />
         Your browser does not support the video tag.
       </video>
     </div>
